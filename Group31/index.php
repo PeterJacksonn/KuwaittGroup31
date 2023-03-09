4df3123357a50df7b5ef65c8f1d6a850ad0e6c7b
Tobias <113984534+orbitalfish@users.noreply.github.com>
2023-03-09 15:50:45 +0000

    Update index.php

--- Group31/index.php
+++ Group31/index.php
@@ -3,11 +3,13 @@ require("navBar.php");
 
 ?>
 
-
 <h1> Kuwait file management system </h1>
 
+<!--main content in DIV below-->
+<div class="container">
+  
 
-
+</div> <!--end of container-->
 
 
 <?php require("footer.php"); ?>
