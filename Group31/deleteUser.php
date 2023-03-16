116609d913e0b5dc5fbf6c2e17a0147179e2b99a
Tobias <trngerem@gmail.com>
2023-03-16 15:45:23 +0000

    del and update

--- Group31/deleteUser.php
+++ Group31/deleteUser.php
@@ -7,7 +7,7 @@ require("adminNav.php");
 
 <h1><u> Delete User Page </u></h1>
 
-
+<a>Delete user <?php echo $_GET['uid'];?>?</a>
 
 
 
--- Group31/updateUser.php
+++ Group31/updateUser.php
@@ -7,7 +7,7 @@ require("adminNav.php");
 
 <h1><u>Update user page</u></h1>
 
-
+<a>Update user <?php echo $_GET['uid'];?>?</a>
 
 
 
