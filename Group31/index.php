7b6ad795893ced4298a70749b43131d648b7f83a
charlotte <charlotte.falby03@gmail.com>
2023-03-17 14:20:50 +0000

    ui changes

--- Group31/index.php
+++ Group31/index.php
@@ -2,9 +2,22 @@
 require("navBar.php");
 
 ?>
+<!-- <a href="https://www.freepik.com/free-vector/digital-technology-background-with-abstract-wave-border_16367386.htm#query=white%20pattern&position=37&from_view=keyword&track=ais">Image by rawpixel.com</a> on Freepik -->
 
+<html>
 
-<h1> Kuwait file management system </h1>
+<head>
+    <style>
+        body {
+            background-image: url("whiteBkg.jpg");
+        }
+    </style>
+</head>
+
+<div>
+    <h1 class="titlefont">Kuwait Document Management System </h1>
+    <img src="documentPic.png" class="image1">
+</div>
 
 
 
--- Group31/navBar.php
+++ Group31/navBar.php
@@ -1,33 +1,33 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Kuwait</title>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="site.css" />
-	
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
-  
-  </head>
+<head>
+	<title>Kuwait</title>
 
-<body class="bgColor">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="site.css" />
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body class="bkg">
 	<header>
-		<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+		<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom box-shadow mb-3" style="background-color:#128754">
 			<div class="container-fluid">
-				<a class="navbar-brand" href="index.php">Home</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
-						aria-expanded="false" aria-label="Toggle navigation">
+				<a class="navbar-brand" style="color: #fff" href="index.php">Home</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
 					<ul class="navbar-nav flex-grow-1">
 						<li class="nav-item">
-							<a class="nav-link text-dark" href="adminLogin.php">Staff Login</a>
-							<li class="nav-item">
-							<a class="nav-link text-dark" href="bUserLogin.php">User Login</a>
+							<a class="nav-link text-light" href="adminLogin.php">Staff Login</a>
+						<li class="nav-item">
+							<a class="nav-link text-light" href="bUserLogin.php">User Login</a>
 						</li>
 					</ul>
 				</div>
