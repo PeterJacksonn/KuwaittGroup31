ae7f844eebf31e9486a047e57753ebc33d2d543e
charlotte <charlotte.falby03@gmail.com>
2023-03-19 18:13:28 +0000

    ui

--- Group31/navBar.php
+++ Group31/navBar.php
@@ -14,12 +14,14 @@
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
+<!-- Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=308936">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=308936">Pixabay</a>-->
 
 <body class="bkg">
 	<header>
 		<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom box-shadow mb-3" style="background-color:#128754">
 			<div class="container-fluid">
-				<a class="navbar-brand" style="color: #fff" href="index.php">Home</a>
+				<a href="index.php"><img src="homeIcon.png" class="image3"></a>
+				<a class="navbar-brand text-light" href="index.php">Home</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
