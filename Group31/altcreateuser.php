8af3c301798e67ea361f31ced55fc75341aa97a1
SSupernova2003 <113983752+SSupernova2003@users.noreply.github.com>
2023-03-24 15:39:00 +0000

    Add files via upload

---
+++ Group31/altcreateuser.php
@@ -0,0 +1,66 @@
+
+<style>
+    .formelement {    
+        background: rgb(255, 255, 255);
+        display: block;
+        margin: 20px auto;
+        text-align: center;
+        border: 4px solid #128754;
+        padding: 14px 10px;
+        height: 50px;
+        width: 500px;
+        outline: none;
+        color: #000000;
+        transition: 0.25s;
+        font-family: monospace;
+        font-size: 20px;
+        }
+</style>
+
+<div class="container bgColor">
+    <main role="main" class="pb-3">
+    <h1> CREATE NEW USER: </h1>
+
+        <form method = "post">
+
+        <?php
+            for($i = 0;$i<(TableColumns($tablename)+1);$i++):
+
+                $columnname = TableNames($tablename,$i);
+                if($columnname == "role"){
+                    echo "
+                    <div>
+                    ".$columnname.":
+                    <select  name='".$columnname."'>
+                    <option value='admin'>admin</option>
+                    <option value='manager'>manager</option>
+                    <option value='active'>active</option>
+                    <option value='staff'>staff</option>
+                    </div>";
+                }
+                elseif($i==TableColumns($tablename)){
+                    echo "
+            
+                    <div>
+                    <input type='submit' name='submit'>
+                    </div>
+                    ";
+                }
+                else{
+                    echo "
+                    <div>
+                    ".$columnname.":
+                    <input class='formelement' type='text' name='".$columnname."'>
+                    </div>
+                    ";
+                }
+
+            endfor;
+            
+        ?>
+
+        </form>
+
+        <?php //echo var_dump($data);  //debug    ?> 
+    </main>
+</div>
