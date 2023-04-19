0df66382dbaf1f0fd257c5b086c057e876741525
adrux444 <91118373+adrux444@users.noreply.github.com>
2023-04-19 19:43:15 +0100

    update docs

--- Group31/userDocument.php
+++ Group31/userDocument.php
@@ -54,7 +54,16 @@ $document = getDocument();
               <td class="tbContents"><?php echo $document[$i]['owner'] ?></td>
               <td class="tbContents"><?php echo $document[$i]['criticality'] ?></td>
               <td class="tbContents"><?php echo $document[$i]['viewers'] ?></td>
-              <td class="tbContents"><a href="adminViewdoc.php?uid=<?php echo $document[$i]['docID']; ?>" class="btn btn-action">View</a></td>
+              <?php session_start();
+              if ($document[$i]['criticality'] == 'low'):?>
+                <td class="tbContents"><a href="adminViewdoc.php?uid=<?php echo $document[$i]['docID']; ?>" class="btn btn-action">View</a></td>
+              <?php endif;
+              if (($document[$i]['criticality'] == 'medium') && ($_SESSION['role'] == 'admin' || $_SESSION['role'] == 'manager')):?>
+                <td class="tbContents"><a href="adminViewdoc.php?uid=<?php echo $document[$i]['docID']; ?>" class="btn btn-action">View</a></td>
+              <?php endif;
+              if (($document[$i]['criticality'] == 'high') && ($_SESSION['role'] == 'manager')):?>
+                <td class="tbContents"><a href="adminViewdoc.php?uid=<?php echo $document[$i]['docID']; ?>" class="btn btn-action">View</a></td>
+              <?php endif; ?>
             </tr>
           <?php endfor; ?>
         </table>
